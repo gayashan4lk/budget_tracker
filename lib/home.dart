a95880d906162793ba3d5665872c22151f5ba092
gayashan4lk <gayashan4lk@gmail.com>
2023-12-17 17:29:10 +0530

    Add new expenses to dummyExpenses list

--- lib/home.dart
+++ lib/home.dart
@@ -14,7 +14,14 @@ class _HomePageState extends State<HomePage> {
   final List<Expense> _dummyExpenses = [
     Expense(title: "Flutter course", amount: 19.99, date: DateTime.now(), category: Category.personal),
     Expense(title: "Lunch", amount: 5.00, date: DateTime.now(), category: Category.food),
-    Expense(title: "Train", amount: 2.50, date: DateTime.now(), category: Category.transportation),
+    Expense(title: "Train", amount: 2.50, date: DateTime.now(), category: Category.personal),
+    Expense(title: "Shopping", amount: 15.50, date: DateTime.now(), category: Category.other),
+    Expense(title: "Gas", amount: 30.00, date: DateTime.now(), category: Category.transportation),
+    Expense(title: "Movie", amount: 12.75, date: DateTime.now(), category: Category.other),
+    Expense(title: "Dinner", amount: 20.00, date: DateTime.now(), category: Category.food),
+    Expense(title: "Books", amount: 40.25, date: DateTime.now(), category: Category.personal),
+    Expense(title: "Gym membership", amount: 50.00, date: DateTime.now(), category: Category.personal),
+    Expense(title: "Concert tickets", amount: 75.50, date: DateTime.now(), category: Category.other),
   ];
 
   @override
