5b8c91c5535659d75c455ded3c479c3e78ea5583
gayashan4lk <gayashan4lk@gmail.com>
2023-12-17 16:18:13 +0530

    Add category enum to Expense model

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,15 +1,19 @@
 import 'package:uuid/uuid.dart';
 
+enum Category { food, transportation, personal, other }
+
 class Expense {
   Expense({
     String? id,
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = id ?? const Uuid().v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
